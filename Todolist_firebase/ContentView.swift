e9af49fbcc122a90025f908d2f7743b52065e073
zi9987 <peterluoluo25@gmail.com>
2024-05-05 00:42:21 +0800

    Initial Commit

---
+++ Todolist_firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todolist_firebase
+//
+//  Created by 羅子淵 on 2024/5/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todolist_firebase/Todolist_firebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todolist_firebaseApp.swift
+//  Todolist_firebase
+//
+//  Created by 羅子淵 on 2024/5/5.
+//
+
+import SwiftUI
+
+@main
+struct Todolist_firebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
