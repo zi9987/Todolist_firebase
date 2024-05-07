9c68d477e179a7609af2f0e6873429426e84bd42
zi9987 <peterluoluo25@gmail.com>
2024-05-08 03:22:45 +0800

    Finished!

--- Todolist_firebase/Views/ProfileView.swift
+++ Todolist_firebase/Views/ProfileView.swift
@@ -9,17 +9,68 @@ import SwiftUI
 
 struct ProfileView: View {
     @StateObject var viewModel = ProfileViewViewModel()
-   
+    
     var body: some View {
         NavigationView{
             VStack{
-                
+                if let user = viewModel.user{
+                    profile(user:user)
+                } else{
+                    Text("Loading Profile ...")
+                }
             }
+            //Sign out
+           
             .navigationTitle("Profile")
         }
+        .onAppear{
+            viewModel.fetchUser()
+        }
+    }
+    @ViewBuilder
+    func profile(user:User) -> some View{
+        //Avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125,height: 125)
+            .padding()
+        //Info
+        VStack(alignment: .leading){
+            HStack{
+                Text("Name:")
+                    .bold()
+                Text(user.name)
+                
+            }
+            .padding()
+            HStack{
+                Text("Email:")
+                    .bold()
+                Text(user.email)
+                
+            }
+            .padding()
+            HStack{
+                Text("Member since:")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                
+            }
+            .padding()
+        }
+        .padding()
+        Button("log Out"){
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        Spacer()
     }
 }
+    
+    #Preview {
+        ProfileView()
+    }
 
-#Preview {
-    ProfileView()
-}
